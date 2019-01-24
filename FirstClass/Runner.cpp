268b3049b3a1f17f8bb62face49ac6f18d289839
Guillermo <memo33101@gmail.com>
2019-01-24 08:24:08 -0700

    Added runner

---
+++ FirstClass/Runner.cpp
@@ -0,0 +1,14 @@
+//
+//  Runner.cpp
+//  FirstClass
+//
+//  Created by Ramos aguirre, Guillermo on 1/24/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+int main()
+{
+    
+    
+    return 0;
+}
