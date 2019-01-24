716b85f4f87c6f2d67547591629cd9f8dd7c5031
Guillermo <memo33101@gmail.com>
2019-01-24 08:30:25 -0700

    Created Controller class

---
+++ FirstClass/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  FirstClass
+//
+//  Created by Ramos aguirre, Guillermo on 1/24/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ FirstClass/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  FirstClass
+//
+//  Created by Ramos aguirre, Guillermo on 1/24/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+
+#endif /* Controller_hpp */
